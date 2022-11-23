1a5c066cfc6862f75ba8229af5bb2121f0cdcd90
flavinus <>
2022-11-23 20:42:33 +0100

    remove renderer

--- src/main/java/fr/flavinus/graph/App.java
+++ src/main/java/fr/flavinus/graph/App.java
@@ -6,15 +6,12 @@ public class App
     {
         Graph graph = Samples.sample2();
 
-        System.out.println(GraphRenderer.render(graph));
-
         graph.findPath(11, 0);
         // Found path for 11 to 0 : 11 6 0
         // ok
         
         graph.findPath(11, 1);        
-        // Found path for 11 to 1 : 11 6 7 8 9 0 1
-        // not the best
+        // Found path for 11 to 1 : 11 6 0 1
 
         //graph.delLink(0, 1);
     }
